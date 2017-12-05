239562cc0f4e43ab3aba4d7d6978feab7f9b8510
Reshma Hussain <reshnaz91@gmail.com>
2017-12-05 22:03:46 +0530

    Commit

--- src/constructor.js
+++ src/constructor.js
@@ -1,17 +1,19 @@
+// Below function is called on button click from html.
 function callThis() {
-    var tsIsFun = (function () {
+    var tsIsFun = /** @class */ (function () {
+        // Our ocnstructor
         function tsIsFun(assgn, count) {
             this.asnmnt = assgn;
             this.asnmntCount = count;
             this.tsAbout = "TypeScript is a strict superset of JavaScript.";
         }
+        // Below function returns output with string and number to be displayed.
         tsIsFun.prototype.describe = function () {
             return this.tsAbout + " This is " + this.asnmnt + ". We have completed " + this.asnmntCount + " assignments so far, including session-1.";
         };
         return tsIsFun;
     }());
-    var tsval = new tsIsFun("Session-2, Assignment-2", 5);
-    var result = tsval.describe();
-    document.getElementById("displayResult").innerHTML = result;
+    var tsval = new tsIsFun("Session-2, Assignment-2", 5); //Object of class
+    var result = tsval.describe(); //Assign output string to variable
+    document.getElementById("displayResult").innerHTML = result; //Display result
 }
-//# sourceMappingURL=constructor.js.map
--- src/constructor.ts
+++ src/constructor.ts
@@ -1,21 +1,24 @@
+// Below function is called on button click from html.
 function callThis() {
 class tsIsFun{
-    asnmnt:string;
-    asnmntCount:number;
-    private tsAbout:string;
+    asnmnt:string; // Constructor takes this string as argument
+    asnmntCount:number; // Constructor takes this number as argument
+    private tsAbout:string; //Private member that gets value assigned in contructor
 
+    // Our ocnstructor
     constructor(assgn:string,count:number){
         this.asnmnt=assgn;
         this.asnmntCount=count;
         this.tsAbout="TypeScript is a strict superset of JavaScript.";
     }
 
+    // Below function returns output with string and number to be displayed.
     describe():string {
         return `${this.tsAbout} This is ${this.asnmnt}. We have completed ${this.asnmntCount} assignments so far, including session-1.`;       
     }
 }
 
-let tsval = new tsIsFun(`Session-2, Assignment-2`, 5);
-let result:string = tsval.describe();
-document.getElementById("displayResult").innerHTML = result;
+let tsval = new tsIsFun(`Session-2, Assignment-2`, 5); //Object of class
+let result:string = tsval.describe(); //Assign output string to variable
+document.getElementById("displayResult").innerHTML = result; //Display result
 }
